f894431a6e8fc2e1ddedcc384b386f9a7781b2bc
Lait-au-Cafe <laitaucafe8@gmail.com>
2020-05-16 13:42:05 +0900

    Generate dummy pages for twitter share.

--- gen_btn_pages.py
+++ gen_btn_pages.py
@@ -24,6 +24,36 @@ anchor_top = int(container.get_attribute('offsetTop'))
 anchor_left = int(container.get_attribute('offsetLeft'))
 print(f"anchor: ({anchor_top}, {anchor_left})")
 
+btn_page_tmpl = """
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>朝比奈非公式bot（美夜ボタン）</title>
+
+  <!-- OG -->
+  <meta name="twitter:card" content="summary"/>
+  <meta name="twitter:image" content="https://lait-au-cafe.github.io/asahina_bot/img/btns/{btn_name}.png"/>
+  <meta name="og:url" content="https://lait-au-cafe.github.io/asahina_bot/"/>
+  <meta name="og:title" content="朝比奈非公式bot（美夜ボタン）"/>
+  <meta name="og:description" content="このサイトはVTuber朝比奈美夜さんのファンサイトです．Twitchの配信からの切り抜き及び朝比奈さんから直接いただいた素材を利用しています．"/>
+  <meta name="og:image:secure_url" content="https://lait-au-cafe.github.io/asahina_bot/img/btns/{btn_name}.png"/>
+  <meta name="og:image:type" content="image/png"/>
+</head>
+<body>
+    <p>このページはTwitterシェア用のダミーページです．</p>
+    <p>通常は本体ページに自動で遷移するようになっています．</p>
+    <p>何らかの原因で遷移しない場合はこちらのリンクから手動で遷移をお願いします．</p>
+    <p><a href="https://lait-au-cafe.github.io/asahina_bot/">https://lait-au-cafe.github.io/asahina_bot/</a></p>
+    <p>いや本当はこんなページ作らずにPHPとか使ってmetaタグ書き分けたほうがいいんだけどね？</p>
+    <p>でもgithub pagesはPHP使えないし，鯖立てるのも管理めんどいし正直勘弁してほしい．マジで．</p>
+    <script>
+        window.location.href = "https://lait-au-cafe.github.io/asahina_bot/";
+    </script>
+</body>
+</html>
+"""
+
 for elem in driver.find_elements_by_class_name("button"):
     # elem.find_element_by_tag_name('p')
     top = anchor_top + int(elem.get_attribute('offsetTop'))
@@ -35,6 +65,9 @@ for elem in driver.find_elements_by_class_name("button"):
     print(f"{audio_filename}: ({top}, {left}, {width}, {height})")
     crop_area = cv2.rectangle(crop_area, (left, top), (left+width, top+height), (0, 0, 255), 2)
     cv2.imwrite(f"img/btns/{audio_filename}.png", image[top:top+height, left:left+width])
+
+    with open(f"btn_pages/{audio_filename}.html", mode='w', encoding='utf-8') as btn_page:
+        btn_page.write(btn_page_tmpl.format(btn_name=audio_filename))
 
     # c = elem.get_attribute('class')
     # voice_type = 0
