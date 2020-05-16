5f817e532bbc6fc885feace4a4b1ad6a9d744589
Lait-au-Cafe <laitaucafe8@gmail.com>
2020-05-16 12:13:30 +0900

    Fix gen_btn_image and generate representative button image.

--- gen_btn_images.py
+++ gen_btn_images.py
@@ -9,24 +9,32 @@ options = webdriver.ChromeOptions()
 options.add_argument('--headless')
 
 driver = webdriver.Chrome(r"D:\Program Files\Selenium\chromedriver.exe", chrome_options=options)
+driver.implicitly_wait(5)
 driver.get(f"file:///{filepath}")
 window_width = driver.execute_script('return document.body.scrollWidth')
 window_height = driver.execute_script('return document.body.scrollHeight')
 driver.set_window_size(window_width, window_height + 100)
 driver.save_screenshot("img/page.png")
 image = cv2.imread("img/page.png")
+crop_area = image.copy()
 print(f"{image.shape}")
+
+container = driver.find_element_by_class_name("container")
+anchor_top = int(container.get_attribute('offsetTop'))
+anchor_left = int(container.get_attribute('offsetLeft'))
+print(f"anchor: ({anchor_top}, {anchor_left})")
 
 for elem in driver.find_elements_by_class_name("button"):
     # elem.find_element_by_tag_name('p')
-    top = int(elem.get_attribute('offsetTop'))
-    left = int(elem.get_attribute('offsetLeft'))
+    top = anchor_top + int(elem.get_attribute('offsetTop'))
+    left = anchor_left + int(elem.get_attribute('offsetLeft'))
     width = int(elem.get_attribute('offsetWidth'))
     height = int(elem.get_attribute('offsetHeight'))
     audio_path = elem.find_element_by_tag_name('audio').get_attribute('src')
     audio_filename = os.path.splitext(os.path.basename(audio_path))[0]
     print(f"{audio_filename}: ({top}, {left}, {width}, {height})")
-    cv2.imwrite(f"img/{audio_filename}.png", image[top:top+height, left:left+width])
+    crop_area = cv2.rectangle(crop_area, (left, top), (left+width, top+height), (0, 0, 255), 2)
+    cv2.imwrite(f"img/btns/{audio_filename}.png", image[top:top+height, left:left+width])
 
     # c = elem.get_attribute('class')
     # voice_type = 0
@@ -37,3 +45,5 @@ for elem in driver.find_elements_by_class_name("button"):
     # else: voice_type = 2
     # dispname = elem.text.replace("\n", "\\n")
     # print(f'{{ "dispname": "{dispname}", "filename": "{os.path.basename(audio_path)}", "type": "{voice_type}" }},')
+
+cv2.imwrite("img/crop.png", crop_area)
